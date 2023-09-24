7878f430a95ff34dcc5ce96748027e1beb158156
Виталий М <vn.mayor@gmail.com>
2023-09-24 20:30:47 +0300

    create mvp2

--- src/tasks/forms.py
+++ src/tasks/forms.py
@@ -1,16 +1,23 @@
+from django import forms
 from django.forms import ModelForm
-
+from .data import TASK_STATUS
 from .models import Task
 
 
 class TaskForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    #    self.fields['status'].widget.attrs['class'] = 'form-control form-select'
+    #    self.fields['due_dt'].widget.attrs['class'] = 'form-control'
+    #    self.fields['created_at'].widget.attrs['class'] = 'form-control dark'
+        self.fields['created_at'].widget.attrs['readonly'] = True
+    #    self.fields['person'].widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Task
-        fields = 'created_at', 'desc_text', 'status', 'due_dt', 'person'
-
-        # widgets = {
-        #     'created_at': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-        #     'desc_text': forms.Textarea(attrs={'type': 'text', 'class': 'form-control'}),
-        #     'status': forms.Textarea(attrs={'class': 'form-control'}),
-        #     'due_dt': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
-        # }
+        fields = '__all__'
+        widgets = {
+            "desc_text": forms.Textarea(attrs={'cols': 20, 'rows': 10}),
+            "due_dt": forms.DateInput(attrs={'type': 'datetime-local'}),
+        }
--- src/tasks/models.py
+++ src/tasks/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.utils import timezone
 from .data import TASK_STATUS
-from datetime import datetime
+
 
 class Task(models.Model):
     """Задачи"""
-    desc_text = models.TextField('Описание задачи', max_length=255)
+    desc_text = models.CharField('Описание задачи', max_length=255)
     status = models.CharField('Статус', max_length=30, choices=TASK_STATUS, default='Нужно сделать')
-    created_at = models.DateField('Дата создания', blank=True, null=True)
-    due_dt = models.DateField('Сделать до', blank=True, null=True)
-    person = models.CharField('Участник', blank=True, null=True, max_length=100)
+    created_at = models.DateTimeField('Дата создания', default=timezone.now)
+    due_dt = models.DateTimeField('Сделать до', null=True, blank=True,)
+    person = models.CharField('Участник', null=True, blank=True, max_length=100, default='')
 
     class Meta:
         db_table = 'task'
+        ordering = ['-created_at']
